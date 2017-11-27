c2c5c04d63ada196469368c2851b40738e810db9
qq20004604 <20004604@qq.com>
2017-11-27 23:06:38 +0800

    开始更新【4、出口】

---
+++ 4、出口/bar.js
@@ -0,0 +1,8 @@
+/**
+ * Created by 王冬 on 2017/11/27.
+ * QQ: 20004604
+ * weChat: qq20004604
+ */
+export default function bar () {
+    console.log('bar')
+}
---
+++ 4、出口/webpack.config.js
@@ -0,0 +1,9 @@
+﻿module.exports = {
+    // 入口文件，指向app.js
+    entry: './app.js',
+    // 出口文件
+    output: {
+        path: __dirname + '/dist',
+        filename: 'dist.chunkhash=[chunkhash:10].name=[name].id=[id].js'
+    }
+}
