ae079fed0e13107212182fd5a871863f1ca2925f
qq20004604 <20004604@qq.com>
2017-12-05 21:02:58 +0800

    开始更新postcss-loader

--- 5、Loader/postcss_loader/src/app.js
+++ 5、Loader/postcss_loader/src/app.js
@@ -4,24 +4,4 @@
  * weChat: qq20004604
  */
 // 引入css文件
-import style from './style/style.css'
-
-console.log(style)
-
-/* useable（开始） */
-let isUse = false
-
-document.querySelector('#test').onclick = function () {
-    if (isUse) {
-        style.unref()
-    } else {
-        style.ref()
-    }
-    isUse = !isUse
-}
-/* useable（结束） */
-
-// import baz from './style/baz.css'
-
-// console.log(baz)
-// document.querySelector('.bottom').innerHTML = baz.abcDef
+import './style/style.css'
