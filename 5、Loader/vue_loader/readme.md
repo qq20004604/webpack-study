3759824f4a584e5cc74bf44c8d347bddea7e708b
王冬 <20004604@qq.com>
2017-12-26 17:49:46 +0800

    开始更新vue-loader

---
+++ 5、Loader/vue_loader/readme.md
@@ -0,0 +1,65 @@
+﻿<h2>VUE-LOADER配置简述</h2>
+
+<h3>前注：</h3>
+
+文档全文请查看 [根目录的文档说明](https://github.com/qq20004604/webpack-study)。
+
+如果可以，请给本项目加【Star】和【Fork】持续关注。
+
+有疑义请[点击这里](https://github.com/qq20004604/webpack-study/issues)，发【Issues】。
+
+[DEMO](https://github.com/qq20004604/webpack-study/tree/master/5%E3%80%81Loader/vue_loader)地址
+
+<h3>0、使用说明</h3>
+
+首先你得会用 Vue.js，不然可能对本文部分内容无法理解
+
+安装：
+
+```
+npm install
+```
+
+运行：
+
+```
+npm run test
+```
+
+单独安装 Vue-loader：
+
+1. 首先要有 Vue；
+2. 其次解析 vue 文件还要 ``vue-template-compiler``；
+3. 解析 css 内容当然还要 ``css-loader``，以及配套的 ``style-loader``。
+
+```
+npm i --s vue
+npm i --s vue-loader
+npm i --s vue-template-compiler
+npm i --s css-loader
+npm i --s style-loader
+```
+
+通过以上安装，至少可以开始使用了
+
+<h3>1、概述</h3>
+
+简单来说，``vue-loader`` 就是处理 ``.vue`` 文件的。
+
+
+<h3>2、配置</h3>
+
+
+<h3>3、问题和解决</h3>
+
+<h4>3.1、引入 vue 但是不能正常运行</h4>
+
+打包后，无法正常运行。查看 console，有以下报错：
+
+```
+You are using the runtime-only build of Vue where the template compiler is not available. Either pre-compile the templates into render functions, or use the compiler-included build.
+```
+
+原因可以参照这个文章[Vue 2.0 升（cai）级（keng）之旅](https://segmentfault.com/a/1190000006435886)。
+
+这里简单总结一下，就是
---
+++ 5、Loader/vue_loader/webpack.config.js
@@ -0,0 +1,58 @@
+﻿// 引入插件
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack')
+
+module.exports = {
+    // 入口文件，指向app.js
+    entry: {
+        dist: './src/index.js'
+    },
+    // 出口文件
+    output: {
+        path: __dirname + '/dist',
+        // 文件名，将打包好的导出为bundle.js
+        filename: '[name].js'
+    },
+    module: {
+        // loader放在rules这个数组里面
+        rules: [
+            {
+                test: /\.js$/,
+                // 这里表示忽略的文件夹，正则语法
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.vue$/,
+                use: [
+                    'vue-loader'
+                ]
+            }
+        ]
+    },
+    // 将插件添加到webpack中
+    plugins: [
+        // 这里是添加的插件
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        }),
+        // new webpack.DefinePlugin({
+        //     'process.env': {
+        //         NODE_ENV: '"production"'
+        //     }
+        // }),
+        // minify with dead-code elimination
+        // new webpack.optimize.UglifyJsPlugin({
+        //     compress: {
+        //         warnings: false
+        //     }
+        // }),
+        // optimize module ids by occurrence count
+        // new webpack.optimize.OccurrenceOrderPlugin()
+    ],
+    resolve: {
+        alias: {
+            'Vue': 'vue/dist/vue.js'
+        }
+    }
+}
