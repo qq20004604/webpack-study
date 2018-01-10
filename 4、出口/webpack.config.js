6769e1cd0de7dcafa7acf112345e61d5594e0d2e
王冬 <20004604@qq.com>
2018-01-10 13:12:27 +0800

    更新使描述准确

--- 4、出口/webpack.config.js
+++ 4、出口/webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
     plugins: [
         // 这里是添加的插件
         new HtmlWebpackPlugin({
-            title: 'title', // html的title（就是title标签里的东西）
+            title: 'title', // html的title（就是title标签里的东西）（不使用自定义模板 template 的时候，这个属性才会生效）
             filename: 'index.html', // 重写后的html文件名，默认是index.html
             template: './demo.html',    // 这个就是那个模板文件，不会改动原有的内容，而是在原来html文件的末尾，将打包编译好的文件添加进去
         })
